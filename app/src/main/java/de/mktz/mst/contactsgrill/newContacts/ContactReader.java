0f5930400fabdbc1ec3137c7c61545bc06828de3
Morgulisan <malte.ktz@hotmail.de>
2019-02-27 13:10:01 +0100

    Code Readability

--- app/src/main/java/de/mktz/mst/contactsgrill/newContacts/ContactReader.java
+++ app/src/main/java/de/mktz/mst/contactsgrill/newContacts/ContactReader.java
@@ -5,12 +5,10 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.Manifest;
-import android.database.ContentObservable;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.CommonDataKinds.*;
 import android.support.v4.app.ActivityCompat;
-import android.util.Log;
 
 
 public class ContactReader {
@@ -28,9 +26,9 @@ public class ContactReader {
 
         ContentResolver contentResolver = context.getContentResolver();
         Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, "_ID = '" + id + "'", null, null, null);
+
+        if(cursor == null) return null;
         ContactWrapper cw = null;
-
-
         if (cursor.moveToFirst()) {
             cw = new ContactWrapper(id, cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
             loadContactNumbers(cw);
@@ -56,24 +54,26 @@ public class ContactReader {
     private void loadContactNumbers(ContactWrapper cw){
         //TODO Projection
         ContentResolver contentResolver = context.getContentResolver();
-        Cursor phones = contentResolver.query(Phone.CONTENT_URI, null, Phone.CONTACT_ID + " = " + cw.getDeviceContactId(), null, null);
-        while (phones.moveToNext()) {
-            String number = phones.getString(phones.getColumnIndex(Phone.NUMBER));
-            int type = phones.getInt(phones.getColumnIndex(Phone.TYPE));
+        Cursor phonesCursor = contentResolver.query(Phone.CONTENT_URI, null, Phone.CONTACT_ID + " = " + cw.getDeviceContactId(), null, null);
+        if(phonesCursor == null) return;
+        while (phonesCursor.moveToNext()) {
+            String number = phonesCursor.getString(phonesCursor.getColumnIndex(Phone.NUMBER));
+            //int type = phonesCursor.getInt(phonesCursor.getColumnIndex(Phone.TYPE));
             cw.addPhoneNumber(number);
         }
-        phones.close();
+        phonesCursor.close();
     }
     private void loadContactPhotos(ContactWrapper cw){
         //TODO Projection
         ContentResolver contentResolver = context.getContentResolver();
-        Cursor photos = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null , ContactsContract.Contacts._ID +" = " + cw.getDeviceContactId(),null,null );
-        if (photos.moveToFirst()) {
-            String photo = photos.getString(photos.getColumnIndex(ContactsContract.Contacts.PHOTO_URI));
-            String icon = photos.getString(photos.getColumnIndex(ContactsContract.Contacts.PHOTO_THUMBNAIL_URI));
+        Cursor photosCursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null , ContactsContract.Contacts._ID +" = " + cw.getDeviceContactId(),null,null );
+        if(photosCursor == null) return;
+        if (photosCursor.moveToFirst()) {
+            String photo = photosCursor.getString(photosCursor.getColumnIndex(ContactsContract.Contacts.PHOTO_URI));
+            String icon = photosCursor.getString(photosCursor.getColumnIndex(ContactsContract.Contacts.PHOTO_THUMBNAIL_URI));
             cw.setPhotoUris(photo,icon);
         }
-        photos.close();
+        photosCursor.close();
     }
     private void loadContactDates(ContactWrapper cw){
         ContentResolver contentResolver = context.getContentResolver();
@@ -82,18 +82,27 @@ public class ContactReader {
                 ContactsContract.CommonDataKinds.Event.START_DATE,
                 ContactsContract.CommonDataKinds.Event.TYPE
         };
-        String selection = ContactsContract.Data.MIMETYPE + " = '" + ContactsContract.CommonDataKinds.Event.CONTENT_ITEM_TYPE + "' AND " + ContactsContract.Contacts.Data.RAW_CONTACT_ID + " = " + cw.getDeviceContactId();
-        Cursor dates = contentResolver.query(ContactsContract.Data.CONTENT_URI,projection, selection,null,null);
-        if(dates.moveToFirst()){
-            int dateType = dates.getInt(dates.getColumnIndex(ContactsContract.CommonDataKinds.Event.TYPE));
-            String dateDate = dates.getString(dates.getColumnIndex(ContactsContract.CommonDataKinds.Event.START_DATE));
-            if(dateType == ContactsContract.CommonDataKinds.Event.TYPE_BIRTHDAY){
-                cw.setBirthday(dateDate);
-            }
-            //cw.DEBUG_Log();
-            //Log.d("malte","New Date of Type " + dateType + " on the " + dateDate + " on contact ID " + dates.getString(dates.getColumnIndex(ContactsContract.Data.RAW_CONTACT_ID)));
+        String selection = ContactsContract.Data.MIMETYPE + " = '" + Event.CONTENT_ITEM_TYPE + "' AND " + ContactsContract.Contacts.Data.RAW_CONTACT_ID + " = " + cw.getDeviceContactId();
+        Cursor datesCursor = contentResolver.query(ContactsContract.Data.CONTENT_URI,projection, selection,null,null);
+        if(datesCursor == null) return;
+        if(datesCursor.getColumnCount() > 0 && datesCursor.moveToFirst()){
+            do {
+                int dateType = datesCursor.getInt(datesCursor.getColumnIndex(Event.TYPE));
+                String dateDate = datesCursor.getString(datesCursor.getColumnIndex(Event.START_DATE));
+                if (dateType == Event.TYPE_BIRTHDAY) {
+                    cw.setBirthday(dateDate);
+                }
+                if (dateType == Event.TYPE_ANNIVERSARY) {
+                    //TODO set event
+                }
+                if (dateType == Event.TYPE_CUSTOM){
+                    
+                }
+                //cw.DEBUG_Log();
+                //Log.d("malte","New Date of Type " + dateType + " on the " + dateDate + " on contact ID " + dates.getString(dates.getColumnIndex(ContactsContract.Data.RAW_CONTACT_ID)));
+            } while (datesCursor.moveToNext());
         }
-        dates.close();
+        datesCursor.close();
     }
 
 
